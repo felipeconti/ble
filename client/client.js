57afae3f9f8505a1ee6d3b2ed175ff01697b9e9f
Felipe Conti <felipeconti18@gmail.com>
2016-01-26 21:48:30 -0200

    implemented post data

--- client/client.js
+++ client/client.js
@@ -1,23 +1,13 @@
 var noble = require('noble');
-var fs = require('fs');
-var FILE = "/tmp/data.json";
+var querystring = require('querystring');
+var http = require('http');
 
-var writeStream = fs.createWriteStream(FILE, {'flags': 'a'});
-
-var UUIDs = {
-	"80e650e44644": "appleTv",
-	"cfb22ab0f82c": "chargerHR",
-	"604cdc67a4c7": "fingerTip",
-	"b4994c73dde8": "beacon"
-};
-
-findUUID = function(object, id){
-	for (UUID in object){
-		if (UUID == id)
-			return object[UUID]
-	}
-	return null;
-};
+// var UUIDs = {
+// 	"80e650e44644": "appleTv",
+// 	"cfb22ab0f82c": "chargerHR",
+// 	"604cdc67a4c7": "fingerTip",
+// 	"b4994c73dde8": "beacon"
+// };
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
@@ -27,16 +17,39 @@ noble.on('stateChange', function(state) {
   }
 });
 
-noble.on('scanStop', function(){
-	writeStream.end(function () { console.log('Done write file '+FILE); });
-});
+noble.on('scanStop', function(){});
 
 noble.on('discover', function(peripheral){
-	// console.log(peripheral.advertisement.localName + " - " + peripheral.rssi + "(" + peripheral.uuid + ")");
-	// 	console.dir(peripheral);
-
-	writeStream.write(Date() + ": " + peripheral + '\n');
-
-	if (findUUID(UUIDs, peripheral.uuid) == "beacon")
-		console.log("Achou o " + peripheral.advertisement.localName + " " + peripheral.uuid + "(RSSI: " + peripheral.rssi + ")");
+	// console.log("Achou o " + peripheral.advertisement.localName + " " + peripheral.uuid + "(RSSI: " + peripheral.rssi + ")");
+	sendBeacon(peripheral);
 });
+
+var sendBeacon = function(data){
+
+	// var data = querystring.stringify({
+	// 	username: user,
+	// 	password: pass
+	// });
+
+	var options = {
+	    host: 'ble.felipeconti.com.br',
+	    port: 80,
+	    path: '/data',
+	    method: 'POST',
+	    headers: {
+	        'Content-Type': 'application/json',
+	        'Content-Length': Buffer.byteLength(data)
+	    }
+	};
+
+	var req = http.request(options, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			console.log("body: " + chunk);
+		});
+	});
+
+	req.write(data);
+	req.end();
+
+};
